01a0cfc7cb8b9ea65ea6e6951633df1ad4bcac2b
naresht1710 <naresht1710@gmail.com>
2020-06-22 12:28:41 +0530

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  render,
+  cleanup,
+  debug,
+  waitFor,
+  findByText,
+  getAllByTestId,
+  findByTestId,
+  fireEvent,
+  getByText,
+  getByLabelText,
+  waitForDomChange
+} from "@testing-library/react";
+
+import App from "./App";
+
+import "@testing-library/jest-dom";
+
+import "@testing-library/jest-dom/extend-expect";
+
+beforeEach(cleanup);
+
+test("component rendered", async () => {
+  let { getByTestId } = render(<App />);
+
+  // await waitFor(() => {
+  //   findByText("Hello CodeSandbox");
+  // });
+  console.log('getByText("Hello CodeSandbox")', getByTestId("test"));
+  expect(getByTestId("test")).toBeInTheDocument();
+});
